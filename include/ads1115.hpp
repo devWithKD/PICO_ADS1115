fc93f9c54252e032922b8f68149c8ec4d565bc7d
Kedar Joshi <kedarjo2002@gmail.com>
2023-02-17 21:07:39 +0530

    Bug Fix

--- include/ads1115.hpp
+++ include/ads1115.hpp
@@ -2,35 +2,35 @@
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 
-#define conversionReg 0x0
-#define configReg 0x1
-#define lowThrReg 0x2
-#define highThrReg 0x3
+#define conversionReg 0x00
+#define configReg 0x01
+#define lowThrReg 0x02
+#define highThrReg 0x03
 
 enum
 {
     COMP_QUE_1 = (0x0000),
     COMP_QUE_2 = (0x0001),
     COMP_QUE_4 = (0x0002),
-    COMP_QUE_DISABLED = (0x0003),
+    COMP_QUE_DISABLED = (0x0003)
 };
 
 enum
 {
     COMP_LAT = (0x0000),
-    COMP_NO_LAT = (0x0004),
+    COMP_NO_LAT = (0x0004)
 };
 
 enum
 {
     COMP_POL_LOW = (0x0000),
-    COMP_POL_HIGH = (0x0008),
+    COMP_POL_HIGH = (0x0008)
 };
 
 enum
 {
     COMP_MODE_TRAD = (0x0000),
-    COMP_MODE_WINDOW = (0x0010),
+    COMP_MODE_WINDOW = (0x0010)
 };
 
 enum
@@ -42,13 +42,13 @@ enum
     DATA_RATE_128 = (0x0080),
     DATA_RATE_250 = (0x00A0),
     DATA_RATE_475 = (0x00C0),
-    DATA_RATE_860 = (0x00E0),
+    DATA_RATE_860 = (0x00E0)
 };
 
 enum
 {
     MODE_CONT = (0x0000),
-    MODE_SINGLE_SHOT = (0x0100),
+    MODE_SINGLE_SHOT = (0x0100)
 };
 
 enum
@@ -58,7 +58,7 @@ enum
     PGA_FSR_2048 = (0x0400),
     PGA_FSR_1024 = (0x0600),
     PGA_FSR_512 = (0x0800),
-    PGA_FSR_256 = (0x0A00),
+    PGA_FSR_256 = (0x0A00)
 };
 
 enum
@@ -70,13 +70,13 @@ enum
     MUX_CONFIG_A0_GND = (0x4000),
     MUX_CONFIG_A1_GND = (0x5000),
     MUX_CONFIG_A2_GND = (0x6000),
-    MUX_CONFIG_A3_GND = (0x7000),
+    MUX_CONFIG_A3_GND = (0x7000)
 };
 
 enum
 {
     OS = (0x0000),
-    OS_START_CONV = (0x8000),
+    OS_START_CONV = (0x8000)
 };
 
 constexpr uint16_t MUX_BY_CHANNEL[] = {
@@ -115,33 +115,45 @@ ads::ads(i2c_inst_t *i2c, int sda, int scl, int addr)
     gpio_set_function(_sda, GPIO_FUNC_I2C);
     gpio_set_function(_scl, GPIO_FUNC_I2C);
     gpio_pull_up(_sda);
-    gpio_pull_up(_sda);
+    gpio_pull_up(_scl);
 }
 
 ads::~ads()
 {
 }
 
-void ads::init(int gain = PGA_FSR_6114, int dataRate = DATA_RATE_128)
+void ads::init(int gain = PGA_FSR_6114, int dataRate = DATA_RATE_8)
 {
     m_gain = gain;
     m_dataRate = dataRate;
-    config = OS_START_CONV | m_gain | MODE_CONT |
-             m_dataRate | COMP_MODE_TRAD |
-             COMP_POL_LOW | COMP_NO_LAT |
-             COMP_QUE_DISABLED;
+    
 }
 
 int ads::adcReadSingleEnd(int channel){
-    config = MUX_BY_CHANNEL[channel];
-    uint8_t datatosend[3];
+    config = 0;
+    config = OS_START_CONV | MODE_CONT  | COMP_MODE_TRAD |
+             COMP_POL_LOW | COMP_NO_LAT |
+             COMP_QUE_1;
+    config |= MUX_BY_CHANNEL[channel] | m_gain | m_dataRate;
+    uint8_t datatosend1[3];
+    uint8_t datatosend2[2];
     uint8_t data[2];
-    datatosend[0] = (uint8_t)configReg;
-    datatosend[1] = ((uint16_t)config) >> 8;
-    datatosend[2] = ((uint16_t)config) & 0x00FF;
-    i2c_write_blocking(_i2c,_addr,datatosend,1,false);
-    i2c_read_blocking(_i2c,_addr,datatosend,2,false);
+    datatosend1[0] = (uint8_t)configReg;
+    datatosend1[1] = ((uint16_t)config) >> 8;
+    datatosend1[2] = ((uint16_t)config) & 0x00FF;
+    printf("%08b %08b %08b \n",datatosend1[0],datatosend1[1],datatosend1[2]);
+    i2c_write_blocking(_i2c,_addr,datatosend1,3,false);
+    datatosend2[0] = (uint8_t)lowThrReg;
+    datatosend2[1] = 0x0000;
+    i2c_write_blocking(_i2c,_addr,datatosend2,2,false);
+    datatosend2[0] = (uint8_t)highThrReg;
+    datatosend2[1] = (uint8_t)0x8000;
+    i2c_write_blocking(_i2c,_addr,datatosend2,2,false);
+    uint8_t regRead = (uint8_t)conversionReg;
+    i2c_write_blocking(_i2c,_addr,&regRead,1,false);
+    i2c_read_blocking(_i2c,_addr,data,2,false);
     int rawAdc = (data[0] << 8)|(data[1]);
+    return rawAdc;
 }
 
 float ads::getVoltageSingleEnd(int channel){
--- main.cpp
+++ main.cpp
@@ -6,11 +6,21 @@
 #define SDA 14
 #define SCL 15
 #define addr 0x48 
+#define I2C i2c1
 
 
 
 int main(){
+    stdio_init_all();
+    ads ads1115(I2C,SDA,SCL,addr);
+    ads1115.init();
     
-
+    while(1){
+        int adc = ads1115.adcReadSingleEnd(0);
+        float voltage = ads1115.getVoltageSingleEnd(0);
+        printf("adc: %d voltage: %.4f\n",adc,voltage);
+        sleep_ms(2000);
+    }
+    
 
 }
